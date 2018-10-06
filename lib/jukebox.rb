12d493b04a9a78971bd4f1c24a9c843f13d418c1
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-06 00:25:53 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   users_response = get.chomp
   songs.each_with_index do |song, i|
-  if users_response == song name
+  if users_response.to_i == i
     puts "Playing <song name>"
   elsif 
     puts "Invalid input, please try again"
