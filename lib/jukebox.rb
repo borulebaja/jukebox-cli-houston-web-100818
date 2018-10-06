99738ed8ca1cdbffda28d013b66e87fea73d1b55
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-06 01:43:27 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,19 +24,20 @@ def list(songs)
 end 
 
 
-def play(songs)
-  puts "Please enter a song name or number:"
-  user_request = gets.chomp
-  songs.each_with_index do |song, i|
-    if user_request.to_i == i
-    puts "playing #{songs[i-1]}"
-  elsif user_request == song 
-  puts "playing #{song[i]}"
-else puts "Invalid input, please try again"
-  end 
-end 
-
-end 
+ def play(input = "")
+     if input == "" 
+      puts "What song would you like to play?\n"
+      input = gets.strip
+    end
+    
+    if input.to_i > 0 && input.to_i <= @songs.length
+      index = input.to_i - 1 
+      puts "Now Playing: #{@songs[index]}"
+    elsif @songs.include?(input)
+      puts "Now Playing: #{input}"
+    end
+  end
+end
 
 
 def exit_jukebox
